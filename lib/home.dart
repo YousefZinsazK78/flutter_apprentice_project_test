8acc5f5ab822c9f54dafd5d2c3f9ae37cf654b97
YousefZinsazK78 <yousefzinsazk78@gmail.com>
2022-08-23 15:37:43 +0430

    (modified)->home.dart;(added)->mag1.jpg,card1.jpg

--- lib/home.dart
+++ lib/home.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import './card1.dart';
 
 class Home extends StatefulWidget {
   const Home({Key? key}) : super(key: key);
@@ -8,24 +9,38 @@ class Home extends StatefulWidget {
 }
 
 class _HomeState extends State<Home> {
+  int _selectedIndex = 0;
+
+  static List<Widget> pages = <Widget>[
+    const Card1(),
+    Container(
+      color: Colors.red,
+    ),
+    Container(
+      color: Colors.yellow,
+    ),
+  ];
+
+  void _onItemTapped(int index) {
+    setState(() {
+      _selectedIndex = index;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text(
           'Fooderlich',
-          // 2
           style: Theme.of(context).textTheme.headline6,
         ),
       ),
-      // TODO: Show selected tab
-      body: Center(
-        child: Text('Let\'s get cooking ! ',
-            // 3
-            style: Theme.of(context).textTheme.headline1),
-      ),
+      body: pages[_selectedIndex],
       bottomNavigationBar: BottomNavigationBar(
         selectedItemColor: Theme.of(context).textSelectionTheme.selectionColor,
+        currentIndex: _selectedIndex,
+        onTap: _onItemTapped,
         items: const [
           BottomNavigationBarItem(
             icon: Icon(Icons.card_giftcard),
