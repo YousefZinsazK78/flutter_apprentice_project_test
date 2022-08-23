d00827167fe5d6130e0b3bb58e6a1dff89d21364
YousefZinsazK78 <yousefzinsazk78@gmail.com>
2022-08-23 12:42:06 +0430

    initial fooderlich branch

---
+++ lib/fooderlich.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class FooderlichScreen extends StatelessWidget {
+  const FooderlichScreen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('fooderlich appbar'),
+      ),
+    );
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_application_test/recipe_detail.dart';
-import './recipe.dart';
+import './fooderlich.dart';
 
 void main() {
   runApp(const MyApp());
@@ -20,77 +19,7 @@ class MyApp extends StatelessWidget {
           secondary: Colors.black,
         ),
       ),
-      home: const MyHomePage(title: 'Recipe list'),
-    );
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({Key? key, required this.title}) : super(key: key);
-
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text(widget.title),
-      ),
-      body: SafeArea(
-        child: ListView.builder(
-          itemCount: Recipe.samples.length,
-          itemBuilder: (context, index) {
-            return GestureDetector(
-              onTap: () {
-                Navigator.push(
-                  context,
-                  MaterialPageRoute(
-                    builder: (context) => RecipeDetailScreen(
-                      recipe: Recipe.samples[index],
-                    ),
-                  ),
-                );
-              },
-              child: buildRecipeCard(Recipe.samples[index]),
-            );
-          },
-        ),
-      ),
-    );
-  }
-
-  Widget buildRecipeCard(Recipe recipe) {
-    return Card(
-      elevation: 2.0,
-      shape: RoundedRectangleBorder(
-        borderRadius: BorderRadius.circular(10.0),
-      ),
-      child: Padding(
-        padding: const EdgeInsets.all(16.0),
-        child: Column(
-          children: [
-            Image(
-              image: AssetImage(recipe.imageUrl),
-            ),
-            const SizedBox(
-              height: 14.0,
-            ),
-            Text(
-              recipe.label,
-              style: const TextStyle(
-                fontSize: 20.0,
-                fontWeight: FontWeight.w700,
-                fontFamily: 'Palatino',
-              ),
-            ),
-          ],
-        ),
-      ),
+      home: const FooderlichScreen(),
     );
   }
 }
